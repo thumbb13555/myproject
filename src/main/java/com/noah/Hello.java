d4f4039ef5f371bcdc966d56d338c1cefe5d0fc1
NoahLiu <thumbb13555@gmail.com>
2019-08-18 12:15:49 +0800

    Java and Kotlin variable,data type

--- src/main/java/com/noah/Hello.java
+++ src/main/java/com/noah/Hello.java
@@ -1,10 +1,20 @@
 package com.noah;
 
+import javax.xml.soap.SOAPPart;
+
 public class Hello {
 
     public static void main(String[] args) {
 //        System.out.println("Hello world");
         Person p = new Person();
         p.hello();
+        int age;
+        age = 22;
+        char c = '中';
+        byte b = 30;
+        float weight = 65.5f;
+        boolean enroll = true;
+        String s = new String("ABCDE");
+        System.out.println(enroll);
     }
 }
--- src/main/kotlin/com/noah/Hello.kt
+++ src/main/kotlin/com/noah/Hello.kt
@@ -4,8 +4,17 @@ fun main(args: Array<String>) {
 //    println("Hello kotlin")
     //分號可加可不加
 //    Human().hello()
-    var h = Human()
+    val h = Human()
     h.hello()
+
+//    val age :Int = 19;
+//    age = 20; <-錯誤
+    var age:Int = 25;
+    age = age.plus(1)
+    val weight = 65.5
+    val ecroll : Boolean = true
+    println(age)
+
 }
 class Human{
     fun hello(){
